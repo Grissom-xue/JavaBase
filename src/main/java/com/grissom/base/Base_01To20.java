84530be15967df0d3bcfc9a85f99a96788ca187a
xuezumin <xuezumin@shuwen.com>
2021-10-01 11:10:17 +0800

    Initial commit

---
+++ src/main/java/com/grissom/base/Base_01To20.java
@@ -0,0 +1,13 @@
+package com.grissom.base;
+
+/**
+ * @author 薛祖民
+ * @Description: 课程介绍 以及 Java简介
+ * @date 2021/9/30 10:39 上午
+ */
+public class Base_01To20 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
