7b61c0c6552c1cb4032391880e652ff23ea7cde7
Grissom <757835490@qq.com>
2021-10-07 15:44:37 +0800

    第二次提交  P40

---
+++ src/main/java/com/grissom/base/Base_21To40.java
@@ -0,0 +1,26 @@
+package com.grissom.base;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * describe
+ *
+ * @author Grissom
+ * @since 2021/10/07
+ */
+public class Base_21To40 {
+    @Test
+    public void test001(){
+//        // 制表符
+//        System.out.println("北京\t天津\t上海");
+//
+//        // 换行符
+//        System.out.println("hello \nJack");
+//
+//        System.out.println("\\");
+
+        // 不同系统的表现形式是不同的
+        System.out.println("韩顺平教育\r北京");
+
+    }
+}
